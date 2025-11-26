7ee658a926378342bfe96bd9fe1e15f6561ebe15
Akhil Reddy <akhilreddynagulapally.nle@gmail.com>
2025-11-26 12:59:04 -0800

    using dataframes and series, and performing operations on squirrel data form 2018

--- day-25/main.py
+++ day-25/main.py
@@ -53,13 +53,55 @@ monday=data[data.day=="Monday"]
 # print(mon_temp*9/5+32)
 
 #creating a dataframe from scratch
-raw_data={
-    "names":["ajay","nihal","akhil"],
-    "mode":["chillar","chillar pro max","decent"]
-}
-print(raw_data)
-data_frame=pandas.DataFrame(raw_data)
-print(data_frame)
+# raw_data={
+#     "names":["ajay","nihal","akhil"],
+#     "mode":["chillar","chillar pro max","decent"]
+# }
+# print(raw_data)
+# data_frame=pandas.DataFrame(raw_data)
+# print(data_frame)
+#
+# data_frame.to_csv("dataframetocsv.csv")
 
-data_frame.to_csv("dataframetocsv.csv")
+squirrel_data=pandas.read_csv("2018_Central_Park_Squirrel_Census_Squirrel_Data.csv")
+# # print(type(squirrel_data))
+# count_grey=0---got confused a lot
+# count_red=0---leave from here to the end of this continous comments
+# count_black=0
+# # print(squirrel_data.count(squirrel_data["Primary Fur Color"]=="Red"))
+# # for row in squirrel_data:
+# if "Gray" in squirrel_data["Primary Fur Color"] :
+#     count_grey+=1
+#     # if row["Primary Fur Color"]=="Red":
+#     #     count_red+=1
+#     # if row["Primary Fur Color"]=="Black":
+#     #     count_black+=1
+#
+# print(count_grey) --- till here , leave it
+squirrel_data=pandas.read_csv("2018_Central_Park_Squirrel_Census_Squirrel_Data.csv")
+
+primary_fur_color=squirrel_data["Primary Fur Color"].to_list()
+# print(primary_fur_color.count("Gray"))
+# squirrel_by_primary_fur_color={"Gray":f"{primary_fur_color.count("Gray")}",
+#                                "Red":f"{primary_fur_color.count("Cinnamon")}",
+#                                "Black":f"{primary_fur_color.count("Black")}"}
+squirrel_data=pandas.read_csv("2018_Central_Park_Squirrel_Census_Squirrel_Data.csv")
+
+primary_fur_color=squirrel_data["Primary Fur Color"].to_list()
+# print(primary_fur_color.count("Gray"))
+# squirrel_by_primary_fur_color={"Gray":f"{primary_fur_color.count("Gray")}",
+#                                "Red":f"{primary_fur_color.count("Cinnamon")}",
+#                                "Black":f"{primary_fur_color.count("Black")}"}
+squirrel_by_primary_fur_color={
+    "colors":["Gray","Red","Black"],
+    "count":[primary_fur_color.count("Gray"),primary_fur_color.count("Cinnamon"),primary_fur_color.count("Black")]
+}
+squirrel_by_primary_fur_color_dataframe=pandas.DataFrame(squirrel_by_primary_fur_color)
+squirrel_by_primary_fur_color_dataframe.to_csv("squirrel_count_by_primary_fur_color.csv")
+
+
+
+
+
+
 
