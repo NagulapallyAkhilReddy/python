e324051b1f540b0953441f358644be953e146598
Akhil Reddy <akhilreddynagulapally.nle@gmail.com>
2025-11-26 20:50:25 -0800

    decided the todo

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -9,11 +9,23 @@ usmap.shape(image)
 
 
 
-def load_the_components(x,y):
-    print(x,y)
+# def load_the_components(x,y):
+#     print(x,y)
+
+
+
+# screen.onscreenclick(load_the_components)
 
 user_input=screen.textinput("Do you know all the states in US ?","type the state name")
-# if user_input in
 
-screen.onscreenclick(load_the_components)
+#TODO:1 - convert the guess to title case
+#TODO:2 - check if the guess is among 50 states
+#TODO:3 - write correct guesses on to map
+#TODO:4 - use a loop to allow users to keep guessing
+#TODO:5 - record the correct guesses in a list
+#TODO:6 - Keep track of the score
+
+
+
+
 screen.mainloop()
