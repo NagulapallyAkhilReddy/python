ddad8090c84b6db5e7b2129283dcb7edcc9d4a49
Akhil Reddy <akhilreddynagulapally.nle@gmail.com>
2025-11-27 12:23:29 -0800

    converted the guess in to title case and checked if the guess is present in the 50 states

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -1,6 +1,6 @@
 import turtle
 from turtle import Screen, Turtle
-
+import pandas
 screen=Screen()
 usmap=Turtle()
 image="blank_states_img.gif"
@@ -19,7 +19,15 @@ usmap.shape(image)
 user_input=screen.textinput("Do you know all the states in US ?","type the state name")
 
 #TODO:1 - convert the guess to title case
+guess=user_input.title()
+# print(guess)
 #TODO:2 - check if the guess is among 50 states
+states=pandas.read_csv("50_states.csv")
+# print(type(states))
+list_of_states=states["state"].to_list()
+
+if guess in list_of_states:
+    print(guess + " is present")
 #TODO:3 - write correct guesses on to map
 #TODO:4 - use a loop to allow users to keep guessing
 #TODO:5 - record the correct guesses in a list
