d26d577e1e540cfa7b4cc3daaafe95a15174e072
Akhil Reddy <akhilreddynagulapally.nle@gmail.com>
2025-12-15 17:29:12 -0800

    completed the NATO ALPHABET TASK

---
+++ day-26/main.py
@@ -0,0 +1,13 @@
+#how to iterate over a pandas dataframe
+# import pandas
+#
+# student_dict={
+#     "student":["Akhil","Nihal","Ajay"],
+#     "score":[100,-1000,-10000]
+# }
+#
+# student_dataframe=pandas.DataFrame(student_dict)
+# print(student_dataframe)
+#
+# for (index,row) in student_dataframe.iterrows():
+#     print(row.student)
